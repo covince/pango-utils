3212286122ef1234405dda6d2a166697d99d16ef
Richard Goater <rg19@sanger.ac.uk>
2021-12-07 17:41:48 +0000

    Updates: * remove unused aliases * add BA alias * add WHO variant mapping * tighten validation regex

--- index.js
+++ index.js
@@ -36,9 +36,26 @@ const topologise = (lineages, fillGaps = false) => {
 
 const buildFullTopology = (lineages) => topologise(lineages, true)
 
-const lineageRegex = /^[A-Z]{1,3}(\.[0-9]+)*$/
+const lineageRegex = /^[A-Z]{1,2}(\.[0-9]+)*$/
 
 const isPangoLineage = string => lineageRegex.test(string)
+
+const whoVariants = {
+  'B.1.1.7': 'Alpha',
+  'B.1.351': 'Beta',
+  'B.1.1.28.1': 'Gamma',
+  'B.1.617.2': 'Delta',
+  'B.1.429': 'Epsilon',
+  'B.1.427': 'Epsilon',
+  'B.1.1.28.2': 'Zeta',
+  'B.1.525': 'Eta',
+  'B.1.1.28.3': 'Theta',
+  'B.1.526': 'Iota',
+  'B.1.617.1': 'Kappa',
+  'B.1.1.1.37': 'Lambda',
+  'B.1.621': 'Mu',
+  'B.1.1.529': 'Omicron'
+}
 
 module.exports = {
   aliases,
@@ -46,5 +63,6 @@ module.exports = {
   expandLineage,
   isPangoLineage,
   toAlias,
-  topologise
+  topologise,
+  whoVariants
 }
